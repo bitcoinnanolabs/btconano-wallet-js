dc7346abca25d07c5092e6ff2d5785d8db9f713e
Emerson Pedroso <emerson@bitcoinnano.org>
2024-08-16 08:48:09 -0300

    dificulty

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
-export const SEND_DIFFICULTY = 'fffffff800000000';
-export const RECEIVE_DIFFICULTY = 'fffffe0000000000';
+export const SEND_DIFFICULTY = 'ffc0000000000000';
+export const RECEIVE_DIFFICULTY = 'f000000000000000';
 export const MIN_AMOUNT = 0.00001;
 export const DEFAULT_TIMEOUT = 60 * 1000; // 60 seconds
